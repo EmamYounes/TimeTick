b704e1e2fda42eb43d25ae6d592970bc70225457
Emam <emamAhmed36@gmail.com>
2020-06-24 11:08:53 +0200

    add reciver object

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val mTime = IntentFilter(Intent.ACTION_TIME_TICK)
-        registerReceiver(MyBroadcastReceivers(), mTime)
+        registerReceiver(receiver, mTime)
     }
 
     override fun onDestroy() {
